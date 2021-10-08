7ec11b7132c7bec52a3c9e5813e5d4e9d7da6214
valentinschintler <s51125@edu.campus02.at>
2021-10-08 19:58:59 +0200

    initial setup

--- src/calc.java
+++ src/calc.java
@@ -1,6 +1,20 @@
 public class calc {
 
     public static void main(String[] args) {
+        int i = 1;
+        double d = 2;
+        boolean b = true;
+        char c = 3;
+        String s = "ich";
+
+        System.out.println(int.class);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(s);
+
+
+
 
     }
 
